aed1fd2db837583b6cf2deab87b91c11db829d8a
Tegacreatives <contact@tegacreatives.com>
2023-10-04 23:00:57 +0100

    Rebranded to tedium

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
-        <div className='flex items-center space-x-5'>
-            <Link href="">
-                  <img className="w-44 object-contain cursor-pointer"
-                      src='https://links.papareact.com/yvf'
-                      alt='medium-logo' />
-            </Link>
-            <div className='hidden md:inline-flex items-center space-x-5'>
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
-            </div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-8">
+        <Link href="/">
+          <h1 className="font-semibold text-4xl">Tedium</h1>
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          {/* <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3> */}
         </div>
-        <div className='flex items-center space-x-5 text-green-600'>
-            <h3 className='cursor-pointer'>Sign In</h3>
-            <h3 className='border px-4 py-1 rounded-full border-green-600 cursor-pointer'>Get Started</h3>
-        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3 className="cursor-pointer">Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600 cursor-pointer">
+          Get Started
+        </h3>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,56 @@
-import Head from 'next/head'
-import Header from '../components/Header';
-import Banner from '../components/Banner';
+import Head from "next/head";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
 import { sanityClient, urlFor } from "../sanity";
-import { Post} from "../typings"
-import Link from 'next/link';
+import { Post } from "../typings";
+import Link from "next/link";
 
 interface Props {
-  posts: [Post]
+  posts: [Post];
 }
 
 const Home = ({ posts }: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Medium Blog</title>
+        <title>Tedium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Banner />
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
-       gap-3 md:gap-6 p-2 md:p-6'>
-        {posts.map(post => (
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
+       gap-3 md:gap-6 p-2 md:p-6"
+      >
+        {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className=' border rounded-lg group cursor-pointer overflow-hidden'>
+            <div className=" border rounded-lg group cursor-pointer overflow-hidden">
               <img
-                className='h-60 w-full object-cover group-hover:scale-105 
-                transition-transform duration-200 ease-in-out'
-                src={urlFor(post.mainImage).url()!} alt="" />
-              <div className='flex justify-between p-5 bg-white'>
-              <div>
-                <p className='text-lg font-bold'>{ post.title}</p>
-                <p className='text-sm'>{post.description} by { post.author.name}</p>
-              </div>
-              <img
-                className='w-12 h-12 rounded-full'
-                src={urlFor(post.author.image).url()!} alt="" />
+                className="h-60 w-full object-cover group-hover:scale-105 
+                transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-sm">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="w-12 h-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
               </div>
             </div>
           </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
   const query = `*[_type == "post"]{ 
@@ -56,15 +64,14 @@ export const getServerSideProps = async () => {
    mainImage,
     slug
    }`;
-  
+
   const posts = await sanityClient.fetch(query);
 
   return {
     props: {
       posts,
-    }
-  }
-}
+    },
+  };
+};
 
-export default Home
-
+export default Home;
